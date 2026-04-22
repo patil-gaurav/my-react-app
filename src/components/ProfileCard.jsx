990eb472b1d6f55a86d643f5a9f84289dd8970fa
Gaurav Patil <gaurav24592@gmail.com>
2026-04-22 22:19:53 +0530

    [useState and useEffect] useState and useEffect lifecycle

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,14 +1,38 @@
-function ProfileCard({ name, image, description }) {
-  function handleFollow() {
-    alert(`You follwed name : ${name}`);
+import { useState, useEffect } from "react";
 
+function ProfileCard({ name, image, description }) {
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const bioLength = description.length;
+
+  console.log("Profile Card rendered for : ", name);
+
+  function handleFollow() {
+    setIsFollowing(!isFollowing);
   }
+
+  useEffect(() => {
+    console.log(`${name} mounted`);
+
+    return () => {
+      console.log(`${name} unmounted`);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(`${name} is now ${isFollowing ? "followed" : "unfollowed"}`);
+  }, [isFollowing]);
+
+
   return (
     <div style={{ border: "1px solid #ccc", padding: "10px", width: "200px", margin: "10px"}}>
       <img src={image} alt={name} width="100%" />
       <h3>{name}</h3>
-      <p>{description}</p>
-      <button onClick={handleFollow}>Follow</button>
+      <p>
+        {description}
+        <small> Bio Length : {bioLength}</small>
+      </p>
+      <button onClick={handleFollow}>{ isFollowing ? "Unfollow" : "Follow" }</button>
     </div>
   );
 }
